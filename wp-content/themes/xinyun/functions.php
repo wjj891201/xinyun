1af79f9c2bffb21d3b91aa38d454238ef6e108ef
‘e <it_z517987404@qq.com>
2023-02-27 22:19:23 +0800

    20230227

--- wp-content/themes/xinyun/functions.php
+++ wp-content/themes/xinyun/functions.php
@@ -10,6 +10,80 @@ function xuwen_nav_menus()
         'footer' => __('页脚菜单', 'xuwen')
     ));
 }
+
 add_action('init', 'xuwen_nav_menus');
 
-
+function xuwen_seo()
+{
+    if (is_home())
+    {
+        $title = get_bloginfo('name');
+        $keywords = '星雲歡樂城，星雲，麻將成';
+        $description = get_bloginfo('description');
+    }
+    if (is_category())
+    {
+        $term = get_queried_object();
+        $title = get_field('title', $term);
+        if (!$title)
+        {
+            $title = $term->name;
+        }
+        $keywords = get_field('keywords', $term);
+        if (!$keywords)
+        {
+            $keywords = '';
+        }
+        $description = get_field('description', $term);
+        if (!$description)
+        {
+            $description = $term->description;
+        }
+    }
+    if (is_singular())
+    {
+        $title = get_field('title');
+        if (!$title)
+        {
+            $title = get_the_title();
+        }
+        $keywords = get_field('keywords');
+        if (!$keywords)
+        {
+            $tag = get_the_tags();
+            if (is_array($tag))
+            {
+                $i = 1;
+                foreach ($tag as $v)
+                {
+                    if ($i == 1)
+                    {
+                        $keywords .= $v->name;
+                    }
+                    else
+                    {
+                        $keywords .= '，' . $v->name;
+                    }
+                }
+            }
+        }
+        $description = get_field('description');
+        if (!$description)
+        {
+            $description = get_the_excerpt();
+        }
+    }
+    if (is_search())
+    {
+        
+    }
+    echo '<title>' . $title . '</title>' . PHP_EOL;
+    if ($keywords)
+    {
+        echo '<meta name="keywords" content="' . $keywords . '">' . PHP_EOL;
+    }
+    if ($description)
+    {
+        echo '<meta name="description" content="' . $description . '">' . PHP_EOL;
+    }
+}
---
+++ wp-content/themes/xinyun/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, no-siteapp, must-revalidate">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=no">
+        <meta content="yes" name="apple-mobile-web-app-capable">
+        <meta content="black" name="apple-mobile-web-app-status-bar-style">
+        <meta content="telephone=no" name="format-detection">
+        <meta data-n-head="1" data-hid="keywords" name="keywords" content="">
+        <meta data-n-head="1" data-hid="description" name="description" content="">
+        <meta data-n-head="1" data-hid="anthor" name="anthor" content="">
+        <meta data-n-head="1" data-hid="robots" name="robots" content="">
+        <?php xuwen_seo() ?>
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/style.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/app.css">
+        <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/aos.css">
+    </head>
+    <body>
+        <section class="ant-layout" id="components-layout-demo-fixed">
+            <header class="header ant-layout-header">
+                <div class="top-header flex">
+                    <ul class="navs flex flex-align-center flex-pack-end">
+                        <li class="nav-item nav-item1 active">
+                            <a href="" class="active"></a>
+                        </li>
+                        <li class="nav-item nav-item2">
+                            <a href="" class=""></a>
+                        </li>
+                    </ul>
+                    <ul class="navs flex flex-align-center flex-pack-end">
+                        <li class="nav-item nav-item3 ">
+                            <a href="" class=""></a>
+                        </li>
+                    </ul>
+                </div>
+            </header>
